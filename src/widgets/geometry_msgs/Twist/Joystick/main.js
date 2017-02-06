79a968801bdf4bfb929bb68a896ac2b0a6c3ceaa
Marcel Huber <marcel.huber@th-bingen.de>
2017-02-06 09:37:46 +0100

    joystick teleop imporved, instant response

--- src/widgets/geometry_msgs/Twist/Joystick/main.js
+++ src/widgets/geometry_msgs/Twist/Joystick/main.js
@@ -38,11 +38,33 @@ Joystick.prototype = {
             }
         };
         this.manager = nipplejs.create(options);
-    
+        this.sendMessages = false;
         this.msgLoop = null;
         var self = this;
         this.maxValue = this.manager.get(0).options.size/2;
-        this.msgLoop = setInterval(function () { self.teleopLoop(); }, 100);
+
+        this.manager.on('start', function (evt, data) {
+            self.msgLoop = setInterval(function () { self.teleopLoop(); }, 100);
+        });
+
+        this.manager.on('end', function (evt, data) {
+            var twist = new ROSLIB.Message({
+                angular : {
+                    x : 0,
+                    y : 0,
+                    z : 0
+                },
+                linear : {
+                    x : 0,
+                    y : 0,
+                    z : 0
+                }
+            });
+            self.publishedTopic.publish(twist);
+            clearInterval(self.msgLoop);
+        });
+
+        
 
     },
     load: function(settings) {
@@ -67,7 +89,6 @@ Joystick.prototype = {
         var nipple = this.manager.get(0);
         var myX = nipple.frontPosition.y / this.maxValue;
         var myY = nipple.frontPosition.x / this.maxValue;
-        console.log('myX: '+myX+ ' ----------- myY: '+myY);
         var twist = new ROSLIB.Message({
             angular : {
                 x : 0,
@@ -81,6 +102,5 @@ Joystick.prototype = {
             }
         });
         this.publishedTopic.publish(twist);
-        
     }
 };
